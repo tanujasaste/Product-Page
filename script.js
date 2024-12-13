f96d45b1354ee8e8d7ad03714d7c4386fe76d37f
tanujasaste <tanujasaste9.com>
2024-12-13 21:45:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const Button1 = document.getElementById("first");
+const Button2 = document.getElementById("second");
+const Button3 = document.getElementById("third");
+
+const RedBtn = document.getElementById("Red");
+const GreenBtn = document.getElementById("Green");
+const BlueBtn = document.getElementById("Blue");
+const PinkBtn = document.getElementById("Pink");
+
+const inputQuantity = document.getElementById('inputQuantity')
+const Buy = document.getElementById("BuyNow")
+
+Button1.addEventListener('click',()=>{
+    model.src = "image1.png"
+})
+Button2.addEventListener('click',()=>{
+    model.src = "image2.png"
+})
+Button3.addEventListener('click',()=>{
+    model.src = "image3.png"
+})
